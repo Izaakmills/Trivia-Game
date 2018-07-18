fdaa4aabc494f237bdb5d6450e2c7b3067302215
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-18 18:52:18 -0400

    fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -58,44 +58,45 @@ var gameOn = false;
 var timeRunOut = false;
 // timer object
 var timer = {
-    seconds:0,
-    limit:16,
+    seconds: 0,
+    limit: 16,
 
     start: function () {
         // if (gameOn) {
-            timerID = setInterval(timer.count, 1000)
-            $(".timer").text(timer.seconds);
-            console.log(timer.seconds)
+        timerID = setInterval(timer.count, 1000)
+        $(".timer").text(timer.seconds);
+        console.log(timer.seconds)
         // }
     },
-    count: function (){
-        if(timer.seconds < 16){
-        timer.seconds++
-        $(".timer").text(timer.limit-timer.seconds);
+    count: function () {
+        if (timer.seconds < 16) {
+            timer.seconds++
+            $(".timer").text(timer.limit - timer.seconds);
         } else {
             timer.reset()
             showResult("ran out of time!")
         }
     },
-    reset: function() {
+    reset: function () {
         clearInterval(timerID)
         timer.seconds = 0;
         $(".timer").text(timer.seconds);
     }
 }
-var num= Object.keys(questions)
+var num = Object.keys(questions)
 var numQuestions = num.length
-$(window).on("load", function () {
+function showResult(result) {
+    $(".answer-display").empty()
+    $(".prompt").html("<div>" + result + "</div>")
+    $(".btn-start").html("<p><button type='button' class='btn btn-primary btn-lg btn-start'>Next Question</button></p>")
+    questionCounter = questionCounter + 1;
+    $(".btn-start").css("display", "");
+}
+$(document).ready(function() {
 
 
     // shows user whether or not they get the question right
-    function showResult(result) {
-    $(".answer-display").empty()
-    $(".prompt").html("<div>"+ result+"</div>")
-    $(".btn-start").html("<p><button type='button' class='btn btn-primary btn-lg btn-start'>Next Question</button></p>")
-    questionCounter = questionCounter + 1;
-    $(".btn-start").css("display","");
-    }
+
 
     // start button click initiates gameplay
     $(".btn-start").click(function () {
@@ -109,7 +110,7 @@ $(window).on("load", function () {
         // function for the timer that counts down once the button is clicked, then if user doesnt answer in time they get it wrong and the next question is shown
 
         function displayQuestion(questionNumber) {
-            if (questionNumber < numQuestions){
+            if (questionNumber < numQuestions) {
                 answer = questions[questionNumber].correctAnswer
                 $(".prompt").html("<h2>" + questions[questionNumber].question + "</h2>")
                 $(".answer-display").html("<p><button type='button' class='btn btn-primary btn-lg' id='a'>" + questions[questionNumber].answers.a + "</button></p>")
@@ -121,50 +122,50 @@ $(window).on("load", function () {
             }
         };
         // document.getElementById("#a/b/c"),onclick = userAnswer(this)
-        $(".btn").click(function(){
+        $(".btn").click(function () {
             guess = this.id
-            switch  (guess) {
+            switch (guess) {
 
-            case "a":
-                userAnswer("a")
-                break;
-            case "b":
-                userAnswer("b")
-                break;
-            case "c":
-                userAnswer("c")
-                break;
-            default :
-                break;
+                case "a":
+                    userAnswer("a")
+                    break;
+                case "b":
+                    userAnswer("b")
+                    break;
+                case "c":
+                    userAnswer("c")
+                    break;
+                default:
+                    break;
             }
         });
 
         // calculates whether or not the user guessed correctly
         function userAnswer(guess) {
-                if (gameOn) {
-                    if (guess == answer) {
-                        numCorrect = numCorrect + 1
-                        showResult("Awesome, you guessed right!")
-                    }
-                    else {
-                        showResult("Drats, wrong answer...")
-                    }
+            if (gameOn) {
+                if (guess == answer) {
+                    numCorrect = numCorrect + 1
+                    showResult("Awesome, you guessed right!")
                 }
-                timer.reset()
+                else {
+                    showResult("Drats, wrong answer...")
+                }
+            }
+            timer.reset()
         };
-   
+
         // hides start button when game is in progress
         function hideIntro() {
             $(".btn-start").css("display", "None")
         }
 
-        function finalScreen(){
+        function finalScreen() {
             percentage = (numCorrect / numQuestions) * 100
             $(".prompt").html("<h2> You have completed the tree quiz good job</h2>")
-            $(".answer-display").html("<p> Correct Answers: " + numCorrect+"</p>")
-            $(".answer-display").append("<p> Your winning percentage "+ percentage+"%</p>")
+            $(".answer-display").html("<p> Correct Answers: " + numCorrect + "</p>")
+            $(".answer-display").append("<p> Your winning percentage " + percentage + "%</p>")
             $(".answer-display").append("<p></p>")
-            $(".timer").css("display","none")
+            $(".timer").css("display", "none")
         }
     });//btn start closing tag
 
